234bbe84289c65f542ffda704e53a725801cd0f1
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-08-07 13:08:01 +0000

    feat: add main game functionality

--- static/js/script.js
+++ static/js/script.js
@@ -10,8 +10,29 @@ function drawGameBoard() {
         gameBox.style.display = "flex";
         gameBox.style.justifyContent = "center";
         gameBox.style.alignItems = "center";
+        gameBox.tabIndex = x;
+        gameBox.addEventListener("click", handleClick);
         gameBoard.appendChild(gameBox);
     }
+}
+
+function generateRandomCardOrder() {
+    for (let x=0;x<totalBoxNumber;x++) {
+        let numberNotChosen = true;
+        while (numberNotChosen) {
+            let randomNumber = Math.floor(Math.random() * (totalBoxNumber / 2));
+            if (countPlacement(randomNumber) < 2) {
+                numberPlacement.push(randomNumber);
+                numberNotChosen = false;
+            }
+        }
+    }
+}
+
+function countPlacement(numberChosen) {
+    let numberCount = 0;
+    numberPlacement.forEach(element => element == numberChosen ? numberCount++ : numberCount);
+    return numberCount;
 }
 
 function calculateCardSize() {
@@ -36,10 +57,76 @@ function resizeCards() {
     }
 }
 
+function handleClick(event) {
+    handleInput(event.target.id);
+}
+
+function handleInput(cardId) {
+    let selectedBox = document.getElementById(cardId);
+    if (selectedBox.style.backgroundImage == 'url("' + faceDownImage + '")') {
+        picked.push(cardId);
+        if (picked.length == 1) {
+            selectedBox.style.backgroundImage = 'url("' + faceUpImages[numberPlacement[picked[0].slice(3)]];
+        }
+        else {
+            if (picked.length == 2) {
+                selectedBox.style.backgroundImage = 'url("' + faceUpImages[numberPlacement[(cardId).slice(3)]];
+                if (numberPlacement[picked[0].slice(3)] == numberPlacement[(cardId).slice(3)]) {
+                    picked.length = 0;
+                }
+                else {
+                    setTimeout(function() {
+                        selectedBox.style.background = 'black url("' + faceDownImage + '") no-repeat center center / cover';
+                        document.getElementById(picked[0]).style.background = 'black url("' + faceDownImage + '") no-repeat center center / cover';
+                        picked.length = 0;
+                    }, 1000);
+                }
+            }
+        }
+    }
+}
+
+function preloadImages() {
+    for (let x=0;x<faceUpImages.length;x++) {
+        const imagePreloaded = new Image();
+        imagePreloaded.src = faceUpImages[x];
+    }
+}
+
+function handleKeyPlay(event) {
+    if (keyboardPosition == -1) {
+        keyboardPosition = 0;
+    } else {
+        switch(event.key) {
+            case "Enter":
+                handleInput("box" + keyboardPosition)
+                break;
+            case "Space":
+                handleInput("box" + keyboardPosition)
+                break;
+            case "ArrowLeft":
+                if (keyboardPosition > 0) {
+                    keyboardPosition--;
+                }
+                break;
+            case "ArrowRight":
+                if (keyboardPosition < totalBoxNumber) {
+                    keyboardPosition++;
+                }
+                break;
+        }
+    }
+    (document.getElementById("box" + keyboardPosition)).focus();
+}
+
 const totalBoxNumber = 18;
+let numberPlacement = [];
+let picked = [];
+let keyboardPosition = -1;
 const gameBoard = document.getElementById("gameboard");
 window.addEventListener("resize", resizeCards);
-const faceDownImage = "https://res.cloudinary.com/dp1ehadna/image/upload/v1722961152/card-back_qvymqm.png"
+window.addEventListener("keydown", handleKeyPlay);
+const faceDownImage = "https://res.cloudinary.com/dp1ehadna/image/upload/v1723025181/card-back-transparent_dygt3k.png"
 const faceUpImages = [ 
     "https://res.cloudinary.com/dp1ehadna/image/upload/v1722961152/tennis_qo0gjn.png",
     "https://res.cloudinary.com/dp1ehadna/image/upload/v1722961152/netball_eopq4b.png",
@@ -52,4 +139,6 @@ const faceUpImages = [
     "https://res.cloudinary.com/dp1ehadna/image/upload/v1722961151/basketball_fiftms.png",
 ]
 
+preloadImages();
 drawGameBoard();
+generateRandomCardOrder();

70e6312da75e054823b0c8589b938a90117dca17
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-08-08 16:17:57 +0000

    feat: implement profile screen updates via database

--- playerprofile/views.py
+++ playerprofile/views.py
@@ -22,16 +22,37 @@ def player_profile_page(request):
         rounds_taken = request.POST.get("rounds-taken")
         game_entry = Game(game_player=request.user, game_type_setting=game_type, rounds_to_complete=rounds_taken)
         game_entry.save()
+
+    # Construct statistics from the modal to display in the view
     
     queryset = Game.objects.filter(game_player=request.user)
+    games_count = (Game.objects.filter(game_player=request.user)).count()
 
-    results = []
+    results_average = 0
+    results_most = 0
+    if (games_count > 1):
+        results_least = 1000
+    else:
+        results_least = 0
 
-    #for element in queryset:
-    
-    template_name = 'playerprofile/profile.html'
+    for element in queryset:
+        results_average += element.rounds_to_complete
+        if results_least > element.rounds_to_complete:
+            results_least = element.rounds_to_complete
+        if results_most < element.rounds_to_complete:
+            results_most = element.rounds_to_complete
+
+    results_average = results_average / games_count
+    results_average = round(results_average, 2)
+
+    #template_name = 'playerprofile/profile.html'
 
     return render(
 	    request,
-	    "playerprofile/profile.html",
+        "playerprofile/profile.html", {
+            "results_games": games_count,
+            "results_average": results_average,
+            "results_least": results_least,
+            "results_most": results_most,
+        }
     )

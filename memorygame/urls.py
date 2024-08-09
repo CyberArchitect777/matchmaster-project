b91b221bbd409e0249ef1917ec688835d8b43f5c
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-08-09 09:43:30 +0000

    feat: stop users accessing logged in content without being signed in

--- memorygame/urls.py
+++ memorygame/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [ 
-    path('', views.GamePage.as_view(), name='memorygame'),
+    path('', login_required(views.GamePage.as_view()), name='memorygame'),
 ]
--- playerprofile/urls.py
+++ playerprofile/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [ 
-    path('', views.player_profile_page, name='playerprofile'),
+    path('', login_required(views.player_profile_page), name='playerprofile'),
 ]

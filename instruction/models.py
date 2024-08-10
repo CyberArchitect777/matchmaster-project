2668302357b1922b9a44d6fe7822a170846d91e0
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-08-10 16:56:24 +0000

    feat: connect accordion to database and add edit and delete buttons for superuser

--- instruction/models.py
+++ instruction/models.py
@@ -14,7 +14,7 @@ class Information(models.Model):
     """
 
     update_author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author_information") # One to many relationship between users and information. Author who made the last update
-    title = models.CharField(max_length=200, blank=False, null=False)
+    title = models.CharField(max_length=200, blank=False, null=False, unique=True)
     content = models.TextField(blank=False, null=False)
     updated_on = models.DateTimeField(auto_now=True) # Date and time the entry was last updated
     active = models.IntegerField(choices=ACTIVE_DISPLAY, default=0) # If the entry should be displayed or not.
--- instruction/views.py
+++ instruction/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Information
 
 # Create your views here.
 
@@ -6,9 +7,12 @@ def display_instruction_page(request):
     """
     Displays the instruction page
     """
+
+    information_list = Information.objects.filter(active=1)
     
     return render(
 	    request,
         "instruction/instruction.html", {
+            "information_list": information_list
         }
     )

45dc425e482e4acea4a5eeeb4de0ad8c9ff0cd7c
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2026-05-07 18:21:39 +0100

    convert project to work with sqlite3 and require no external database

--- memorygame/apps.py
+++ memorygame/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
-
+from django.db import connection
 
 class MemorygameConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'memorygame'
+
+    def ready(self):
+        if connection.vendor == "sqlite":
+            try:
+                with connection.cursor() as cursor:
+                    cursor.execute("PRAGMA journal_mode=WAL;")
+            except Exception:
+                pass
